ac03bde72f15805c8f0c57cf12d978a51e182ec7
el <hylee101001@gmail.com>
2018-02-10 23:26:33 -0500

    dd

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ clc, clear, close all
 img = getImage('img0.jpg');
 imshow(img)
 
-[L,N] = superpixels(img,1000);
+[L,N] = superpixels(img,10000);
 
 figure
 BW = boundarymask(L);
@@ -11,5 +11,19 @@ imshow(imoverlay(img,BW,'cyan'))
 
 
 
+outputImage = zeros(size(img),'like',img);
+idx = label2idx(L);
+numRows = size(img,1);
+numCols = size(img,2);
+for labelVal = 1:N
+    redIdx = idx{labelVal};
+    greenIdx = idx{labelVal}+numRows*numCols;
+    blueIdx = idx{labelVal}+2*numRows*numCols;
+    outputImage(redIdx) = mean(img(redIdx));
+    outputImage(greenIdx) = mean(img(greenIdx));
+    outputImage(blueIdx) = mean(img(blueIdx));
+end    
 
+figure
+imshow(outputImage)
 
