abb641dfea48971b806b48f9e44eb898aa3ac857
el <hylee101001@gmail.com>
2018-02-11 04:02:27 -0500

    dd

--- main.m
+++ main.m
@@ -2,9 +2,10 @@ clc, clear, close all
 
 img = getImage('img0.jpg');
 imshow(img)
+greyDummy = rgb2gray(img);
 
-[L,N] = superpixels(img,100);
 
+[L,N] = superpixels(img,10000);
 figure
 BW = boundarymask(L);
 imshow(imoverlay(img,BW,'cyan'))
@@ -18,11 +19,67 @@ for labelVal = 1:N
     redIdx = idx{labelVal};
     greenIdx = idx{labelVal}+numRows*numCols;
     blueIdx = idx{labelVal}+2*numRows*numCols;
-    outputImage(redIdx) = mean(img(redIdx));
-    outputImage(greenIdx) = mean(img(greenIdx));
-    outputImage(blueIdx) = mean(img(blueIdx));
+    
+    r = round(mean(img(redIdx)));
+    g = round(mean(img(greenIdx)));
+    b = round(mean(img(blueIdx)));
+    color(labelVal, :) = [r, g, b];
+    outputImage(redIdx) = r;
+    outputImage(greenIdx) = g;
+    outputImage(blueIdx) = b;
 end    
  
 figure
-% imshow(outputImage)
-imshow(imoverlay(outputImage,BW,'cyan'))
+% imshow(imoverlay(outputImage,BW,'cyan'))
+imshow(outputImage)
+
+color;
+% dlmwrite('MyMatrix.txt', color, 'delimiter','\t')
+
+diffused = importdata('diffused.txt');
+m = diffused(:,1)/8;
+c = diffused(:,2:4)*8;
+mc = m.*c;
+
+md = ones(size(greyDummy),'like',greyDummy)*255;
+cd = ones(size(img),'like',img)*255;
+myoutput = zeros(size(img),'like',img);
+
+for labelVal = 1:N
+    redIdx = idx{labelVal};
+    greenIdx = idx{labelVal}+numRows*numCols;
+    blueIdx = idx{labelVal}+2*numRows*numCols;
+    
+    temp = mc(labelVal,:);
+    md(redIdx) = diffused(labelVal,1);
+    
+    cd(redIdx) = c(labelVal,1);
+    cd(greenIdx) = c(labelVal,2);
+    cd(blueIdx) = c(labelVal,3);
+    
+    myoutput(redIdx) = temp(1);
+    myoutput(greenIdx) = temp(2);
+    myoutput(blueIdx) = temp(3);
+end
+
+
+figure
+imshow(md)
+
+figure
+imshow(cd)
+
+figure
+imshow(myoutput)
+
+
+
+
+
+
+
+
+
+
+
+
