4934752ce6562aed11dbc4a5fe26b509f8a74227
el <hylee101001@gmail.com>
2018-02-12 15:39:31 -0800

    dd

--- Scrips/getImageWithResultData.m
+++
@@ -1,7 +0,0 @@
-function[img, totalData] = getImageWithResultData(name, scale, imgIndex)
-    img = imread(name);
-    [h, w, c] = size(img);
-    img = imresize(img, [h/scale, w/scale]);
-    fname = strcat('frzn_mdcdmscs', int2str(imgIndex+1), '.txt');
-    totalData = importdata(fname);
-end
--- Scrips/showImg.m
+++
@@ -1,5 +0,0 @@
-function[] = showImg(titleContents, img)
-hAxes = axes( figure );
-hImage = imshow( img, 'Parent', hAxes );
-title( hAxes, titleContents);
-end
